e44fa54c91c0e60ef5b3c6035c392294a9dd908a
Roman Pigunov <jerome.malone@yandex.ru>
2026-06-19 22:58:16 +0300

    1

--- scripts/script.js
+++ scripts/script.js
@@ -104,6 +104,8 @@ document.addEventListener("DOMContentLoaded", () => {
   switchingStickinessHeader(bodyElements.title, bodyElements.header); // Липкий выезжающий header
 
   initializeHeaderImages(
+    getImagePath,
+    basicLink,
     currentData,
     bodyElements.headerImages,
     bodyElements.headerImagesCaption,
